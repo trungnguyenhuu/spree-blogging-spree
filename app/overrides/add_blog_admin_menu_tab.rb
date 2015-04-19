9fbb84de827c006886360f2b719383ff1b5e9677
Tim <thogg4@gmail.com>
2015-04-18 23:43:03 -0600

    upgrade markup

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,11 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
-                     :name => "blog_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>",
-                     :disabled => false)
+Deface::Override.new({
+  :virtual_path => "spree/layouts/admin",
+  :name => "blog_admin_tabs",
+  :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+  :text => "
+    <ul class='nav nav-sidebar'>
+      <%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') if can? :manage, Spree::BlogEntry %>
+    </ul>
+  ",
+  :disabled => false
+})
